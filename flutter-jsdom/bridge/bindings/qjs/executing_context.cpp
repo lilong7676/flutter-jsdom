d04322c571a645c64a758e53660c98fa9f1ff367
Longlong Li <lilong7676@outlook.com>
2022-08-23 10:03:15 +0800

    add: quickjs static lib in cmakelists

--- flutter-jsdom/bridge/bindings/qjs/executing_context.cpp
+++ flutter-jsdom/bridge/bindings/qjs/executing_context.cpp
@@ -4,11 +4,34 @@
 
 namespace jsdom::binding::qjs {
 ExecutionContext::ExecutionContext(int32_t contextId, void* owner) : contextId(contextId), owner(owner) {
-  std::cout<<"ExecutionContext init" << std::endl;
+  std::cout << "ExecutionContext init" << std::endl;
 }
 
 bool ExecutionContext::evaluateJavaScript(const char* code, size_t codeLength, const char* sourceURL, int startLine) {
-  std::cout<<"ExecutionContext evaluateJs code:" << code << std::endl;
+  std::cout << "ExecutionContext evaluateJs code:" << code << std::endl;
+
+  // 构造一个 JS 运行时
+  JSRuntime* runtime = JS_NewRuntime();
+  // 根据运行时构建一个上下文环境
+  m_context = JS_NewContext(runtime);
+  // 调用 C 的 std 方法帮助在控制台输出调试信息
+  // js_std_add_helpers(m_context, 0, NULL);
+
+  // const char* scripts = "console.log('hello quickjs')";
+  JSValue result = JS_Eval(m_context, code, codeLength, "main", 0);
+
+  bool success = handleException(&result);
+
+  return success;
+}
+
+bool ExecutionContext::handleException(JSValue* exception) {
+  if (JS_IsException(*exception)) {
+    JSValue error = JS_GetException(m_context);
+    JS_FreeValue(m_context, error);
+    return false;
+  }
+
   return true;
 }
 
--- flutter-jsdom/bridge/bindings/qjs/executing_context.h
+++ flutter-jsdom/bridge/bindings/qjs/executing_context.h
@@ -3,7 +3,7 @@
  * @Author: lilonglong
  * @Date: 2022-08-19 22:03:52
  * @Last Modified by: lilonglong
- * @Last Modified time: 2022-08-22 14:45:22
+ * @Last Modified time: 2022-08-23 09:57:45
  */
 
 #ifndef EXECUTING_CONTEXT_H_
@@ -11,16 +11,22 @@
 
 #include <cstdint>
 #include <stddef.h>
+#include "third_party/quickjs/list.h"
+#include "third_party/quickjs/quickjs.h"
+#include "third_party/quickjs/quickjs-libc.h"
 
 namespace jsdom::binding::qjs {
 class ExecutionContext {
  public:
   ExecutionContext(int32_t contextId, void* owner);
   bool evaluateJavaScript(const char* code, size_t codeLength, const char* sourceURL, int startLine);
+  bool handleException(JSValue* exception);
 
  private:
   int32_t contextId;
   void* owner;
+
+  JSContext* m_context = nullptr;
 
 };
 }  // namespace jsdom::bindings::qjs
